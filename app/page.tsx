88cfbe74bb4e4e585996de1cb6572e09924b6ff3
Talha Salman <talhahsalman-boop>
2026-05-16 12:21:33 +0500

    updated page

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main style={{ padding: "40px", fontFamily: "Arial" }}>
       <h1>Next.js + Docker + TypeScript 🚀</h1>
-      <p>This is a minimal optimized Docker setup.</p>
+      <p>Aisy hi bnai h.</p>
     </main>
   );
 }
